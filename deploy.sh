b5727c04dd454e3cd32743ee6fe3316117f3b7dd
Pawel Zukowski <p.z.idlecode@gmail.com>
2020-10-02 17:01:56 +0200

    Add settings/environment variables management

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 # Generate requirements.txt file
+echo "Generating requirements.txt..."
 poetry export --format requirements.txt > requirements.txt
 
 # Upload source code to the GCP
+echo "Deploying to GCP..."
 gcloud functions deploy \
   count_commits \
   --allow-unauthenticated \
-  --entry-point=count_commits \
+  --entry-point=on_request_received \
   --runtime python38 \
+  --memory=128MB \
+  --env-vars-file=.env.yaml \
   --trigger-http
-#  --env-vars-file=.env \
 
 
--- main.py
+++ main.py
@@ -1,18 +1,55 @@
 #!/usr/bin/env python
+from pathlib import Path
+from typing import List
+
 from flask import Request
+from datetime import date
+
+from pydantic_settings import BaseSettingsModel, load_settings
 
 
-def count_commits(request: Request):
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
+class AppSettings(BaseSettingsModel):
+    # Enable debug output (logging, additional messages, etc)
+    DEBUG: bool = False
+
+    # GitHub access token
+    GITHUB_ACCESS_TOKEN: str = "<YOUR_TOKEN_GOES_HERE>"
+
+    # List of repositories to track in "<user>/<repository>" format separated by a colon ':' character
+    REPOS_TO_WATCH: str = "user1/repo1:user2/repo2"
+
+    @property
+    def REPOS_TO_WATCH_LIST(self) -> List[str]:
+        return self.REPOS_TO_WATCH.split(":")
+
+    # Challenge start and end dates in YYYY-MM-DD format
+    START_DATE: date = date.fromisoformat("2020-10-10")
+    END_DATE: date = date.fromisoformat("2021-10-10")
+
+    # Commits required in challenge
+    REQUIRED_COMMIT_COUNT: int = 365
+
+    class Config:
+        env_prefix = "CHALLENGE"
+
+
+def on_request_received(req: Request):
+    request_json = req.get_json()
+
+    print(f"Start date: {settings.START_DATE}")
+    return f"Repos to watch: {', '.join(settings.REPOS_TO_WATCH_LIST)}"
+
+
+def serve_flask_endpoint(endpoint):
+    from flask import Flask, request
+    app = Flask(__name__)
+    app.debug = True
+    app.route("/")(lambda: endpoint(request))
+    app.run()
 
 
 if __name__ == "__main__":
-    # TODO: start sample server?
-    print(count_commits(Request({})))
-
+    settings = load_settings(AppSettings, Path(".env.yaml"))
+    serve_flask_endpoint(on_request_received)
+else:
+    settings = load_settings(AppSettings, "{}", type_hint="json", load_env=True)
