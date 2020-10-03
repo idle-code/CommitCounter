0d41ec8f4f6d3134a57b4ce7d148bb132aa869af
Pawel Zukowski <p.z.idlecode@gmail.com>
2020-10-03 16:47:05 +0200

    Replace pydantic-settings with simpler yaml loader

--- main.py
+++ main.py
@@ -2,30 +2,30 @@
 from pathlib import Path
 from flask import Request
 import datetime
+from pydantic import BaseSettings
 
-from pydantic_settings import BaseSettingsModel, load_settings
 
-
-class AppSettings(BaseSettingsModel):
+class AppSettings(BaseSettings):
     # Enable debug output (logging, additional messages, etc)
     DEBUG: bool = False
 
     # GitHub access token
     GITHUB_ACCESS_TOKEN: str
 
-    # Challenge start and end dates in YYYY-MM-DD format
-    START_DATE: datetime.datetime = datetime.datetime.now()
-    END_DATE: datetime.datetime = datetime.datetime.now() + datetime.timedelta(days=365)
-
     # Commits required in challenge
     REQUIRED_COMMIT_COUNT: int = 365
+
+    # Challenge start and end dates in YYYY-MM-DD format
+    END_DATE: datetime.datetime = datetime.datetime.now()
+    START_DATE: datetime.datetime = END_DATE - datetime.timedelta(days=REQUIRED_COMMIT_COUNT)
 
 
 def on_request_received(req: Request):
     if "days" in req.args:
         days_to_display = int(req.args["days"])
+        print(f"Looking only {days_to_display} days behind")
         settings.END_DATE = datetime.datetime.now()
-        settings.START_DATE = datetime.datetime.now() - datetime.timedelta(days=days_to_display)
+        settings.START_DATE = settings.END_DATE - datetime.timedelta(days=days_to_display)
 
     print(f"Start date: {settings.START_DATE}")
     print(f"End date: {settings.END_DATE}")
@@ -53,17 +53,15 @@ def fetch_commits():
     all_commits = []
     for repo in repositories:
         commits_in_repo = fetch_commits_in_repo(repo)
-        if commits_in_repo:
-            print(f"Adding {len(commits_in_repo)} commits")
-            all_commits.extend(commits_in_repo)
+        all_commits.extend(commits_in_repo)
 
     print(f"Total commits in found all repositories: {len(all_commits)}")
     return all_commits
 
 
 def fetch_commits_in_repo(repo):
-    print(f"Checking repository {repo}...")
     commits_in_repo = repo.get_commits(since=settings.START_DATE, until=settings.END_DATE, author=repo.owner)
+    print(f"Found {commits_in_repo.totalCount} commits in {repo}")
     return list(commits_in_repo)
 
 
@@ -75,8 +73,20 @@ def serve_flask_endpoint(endpoint):
     app.run()
 
 
+def load_settings_from_environment() -> AppSettings:
+    return AppSettings()
+
+
+def load_settings_from_yaml(yaml_path: str) -> AppSettings:
+    import yaml
+    with open(yaml_path) as yaml_file:
+        yaml_env = yaml.safe_load(yaml_file)
+        return AppSettings(**yaml_env)
+
+
 if __name__ == "__main__":
-    settings = load_settings(AppSettings, Path(".env.yaml"))
+    settings = load_settings_from_yaml(".env.yaml")
     serve_flask_endpoint(on_request_received)
 else:
-    settings = load_settings(AppSettings, "{}", type_hint="json", load_env=True)
+    settings = load_settings_from_environment()
+
